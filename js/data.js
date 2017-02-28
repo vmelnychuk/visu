7fa79098abad9fb730e754d1f8fac5616f79ceca
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2017-02-28 16:02:10 +0100

    add some data

---
+++ js/data.js
@@ -0,0 +1,10 @@
+if (typeof(Domains) === 'undefined') Domains = {};
+
+Domains.list =
+[
+    {"domainName": "cosmos.agency"},
+    {"domainName": "cosmos.tours"},
+    {"domainName": "486made.me"},
+    {"domainName": "космическийтуризм.рус"}
+];
+
